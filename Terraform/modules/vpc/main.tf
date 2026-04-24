b2be6f0829b610e0fabe396e7de06be7ce27d876
kamal17701 <kamalbh1717@gmail.com>
2026-04-24 15:23:06 +0530

    Create main.tf

---
+++ Terraform/modules/vpc/main.tf
@@ -0,0 +1,10 @@
+module "vpc" {
+  source = "./modules/vpc"
+
+  project_name       = var.project_name
+  environment        = var.environment
+  vpc_cidr           = var.vpc_cidr
+  availability_zones = var.availability_zones
+  public_subnets     = var.public_subnets
+  private_subnets    = var.private_subnets
+}
