3eb7cbeb27c459354c1b7458b4b83cb1f20e6515
kamal17701 <kamalbh1717@gmail.com>
2026-06-29 11:59:45 +0530

    Update KamalAppApplication.java

--- src/main/java/.../com/kamal/devops/KamalAppApplication.java
+++ src/main/java/.../com/kamal/devops/KamalAppApplication.java
@@ -1,1 +1,29 @@
+package com.kamal.devops;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Main entry point for the Spring Boot application.
+ *
+ * This class starts the application and enables component scanning
+ * for all classes under the com.kamal.devops package, including:
+ *
+ * - controller
+ * - config
+ * - service (if added later)
+ * - repository (if added later)
+ *
+ * Exposed endpoints:
+ *   GET /health
+ *   GET /info
+ *   GET /actuator/health
+ *   GET /actuator/prometheus
+ */
+@SpringBootApplication
+public class KamalAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KamalAppApplication.class, args);
+    }
+}
